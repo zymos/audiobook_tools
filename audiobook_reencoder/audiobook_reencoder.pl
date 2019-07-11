79c25f5bc8032441b0748264a0d8e3bcaa326bba
ZyMOS <zymos@users.noreply.github.com>
2019-07-11 14:45:40 -0600

    add rm cue, quiet enconder verbose, rm bp3 tmp file

--- audiobook_reencoder/audiobook_reencoder.pl
+++ audiobook_reencoder/audiobook_reencoder.pl
@@ -37,7 +37,7 @@ $DELETE_ORIGINAL = 1;
 $USE_MP3GAIN_FOR_NORMALIZATION = 1;
 $ADD_ID3_AUDIOBOOK_TAG = 1;
 
-
+$DELETE_CUE_FILE = 1;
 
 
 
@@ -70,13 +70,13 @@ sub m4btomp3 {
 	# Grab top directory of file
 	my $top_dir = $File::Find::dir; 
 	$top_dir =~ s/.*\///; 	
-	$file2 = $file;
-	$file2 =~ s/[Mm]4[AaBb]$/mp3/;
+	my $file2 = $file;
+	$file2 =~ s/[Mm]4[AaBb]$/.tmp-for-m4b.mp3/;
 
 	print "# Convert m4b/m4a to mp3...\n";
 	print "#  [$file] to [$file2]\n";
 	
-	$command = "avconv -i \"$file\" -c:v copy \"$file2\""; # c:v copy copies cover art
+	$command = "avconv -loglevel \"fatal\" -i \"$file\" -c:v copy \"$file2\""; # c:v copy copies cover art
 	if(! $TEST ){
 		$out = `$command`;
 	}else{
@@ -95,36 +95,48 @@ sub backup_files{
 	$top_dir =~ s/.*\///; 	
 
 	if($DELETE_ORIGINAL){ # Delete the origional files after encoding
-			print "# Deleting original file...\n";
-			$command = "/bin/rm \"$file\"";
-			if(! $TEST ){
-				#sleep before delete incase you made a 
-				# terible mistake and want to Ctrl-c
-				sleep(3); 
-				$out = `$command`; 
-			}else{
-				print ">>Executing: $command\n";
-			}
-		}elsif( -e $BACKUP_DIR . "/" . $file ){ #Backup original file, if file already exists, move to 
-			 # backup dir as dup filename
-			print "# Moving original file to backup directory...\n";
-			$dup = $BACKUP_DIR . "/" . $file . "-dup.mp3";
-			$command = "mv \"$file\" \"$dup\"";
-			if(! $TEST ){
-				$out = `$command`; 
-			}else{
-				print ">>Executing: $command\n";
-			}
-		}else{ # Backup origional file, move to backup dir
-			print "# Moving original file to backup directory...\n";
-			$command = "mv \"$file\" \"$BACKUP_DIR\"";
-			if(! $TEST ){
-				$out = `$command`;
-			}else{
-				print ">>Executing: $command\n";
-			}
+		print "# Deleting original file...\n";
+		$command = "/bin/rm \"$file\"";
+		if(! $TEST ){
+			#sleep before delete incase you made a 
+			# terrible mistake and want to Ctrl-c
+			sleep(3); 
+			$out = `$command`; 
+		}else{
+			print ">>Executing: $command\n";
+		}
+	}elsif( -e $BACKUP_DIR . "/" . $file ){ #Backup original file, if file already exists, move to 
+		 # backup dir as dup filename
+		print "# Moving original file to backup directory...\n";
+		$dup = $BACKUP_DIR . "/" . $file . "-dup.mp3";
+		$command = "mv \"$file\" \"$dup\"";
+		if(! $TEST ){
+			$out = `$command`; 
+		}else{
+			print ">>Executing: $command\n";
+		}
+	}else{ # Backup origional file, move to backup dir
+		print "# Moving original file to backup directory...\n";
+		$command = "mv \"$file\" \"$BACKUP_DIR\"";
+		if(! $TEST ){
+			$out = `$command`;
+		}else{
+			print ">>Executing: $command\n";
+		}
 	}
 
+	if($DELETE_CUE_FILE){
+		my $cue_file =~ s/\.mp3$/.cue/;
+		if( -e $cue_file )
+		$command = "/bin/rm \"$file\"";
+		if(! $TEST ){
+			#sleep before delete incase you made a 
+			# terrible mistake and want to Ctrl-c
+			$out = `$command`; 
+		}else{
+			print ">>Executing(rm cue file): $command\n";
+		}
+	}
 }
 
 
@@ -132,6 +144,7 @@ sub backup_files{
 # Encode the files
 sub the_operation {
 	my $file = $_;
+	my $file2 = '';
 
 	# Grab top directory of file
 	my $top_dir = $File::Find::dir; 
@@ -158,11 +171,13 @@ sub the_operation {
 	print "#\n";
 
 
-	# process M4B files
+	# preprocess M4B files
+	my $is_m4b=0;
 	if( $file =~ /\.m4b$/i || $file =~ /\.m4a$/i ){
 		$file2 = &m4btomp3($file);
 		&backup_files($file);
 		$file = $file2;
+		$is_m4b=1;
 	}
 
 	# dont encode if its definatly already encoded
@@ -175,63 +190,65 @@ sub the_operation {
 
 		# Create new file name
 		$file2 = $file;
-		$file2 =~ s/\.[Mm][Pp]3$/-32k.mp3/;
+		if( $is_m4b ){
+			$file2 =~ s/\.[Mm]4[AaBb]\.tmp-for-m4b.mp3$/-32k.mp3/;
+		}else{
+			$file2 =~ s/\.[Mm][Pp]3$/-32k.mp3/;
+		}
 	
 		# print "# Encoding: [$file] \n    in [$top_dir]\n";
 		# print "# Processing [$count] of [$total_file_count]\n";
 		$count += 1;
 		
 		# The encoding processes, (lame + id3 + mp3gain + del orginal)
-		if(1){ #legacy crap
-			if(!$NORMALIZATION){
-				$lame_normalization_arguments = "--noreplaygain";
-			}elsif(!$USE_MP3GAIN_FOR_NORMALIZATION){
-				$lame_normalization_arguments = "--replaygain-accurate";
-			}else{
-				$lame_normalization_arguments = "--noreplaygain";
-			}
-			print "# Starting Lame...\n";
-			$lame_command = "lame --resample 22050 --cbr -b 32 $lame_normalization_arguments \"$file\" \"$file2\"";
-			if(! $TEST ){
-				$out = `$lame_command`;
-			}else{
-				print ">>Executing: $lame_command\n";
-			}
-
-			if($? && !$TEST){ # lame encode failed
-				print "# ERROR encoding: $file \n";
-				print FILE "Fail: $file\n";
-				print "# Deleting newly created, failed file\n";
-				$out = `rm -f "$file2"`;
-				print "EXITING (1)\n";
-				exit 1;
-			}else{ #lame sucsseded
-				print FILE "Good: $file2\n";
-				if($ADD_ID3_AUDIOBOOK_TAG){
-					print "# Applying ID3 tag, \"Audiobook\" genre...\n";
-					$command = "id3v2 --TCON \"Audiobook\" \"$file2\"";
-					if(! $TEST ){
-						$out = `$command`; # ID3 tag content->"Audiobook"
-					}else{
-						print ">>Executing: $command\n";
-					}
-				}
-
-				if($USE_MP3GAIN_FOR_NORMALIZATION && $NORMALIZATION){
-					print "# Applying volume normilization...\n";
-					$command = "mp3gain \"$file2\" > /dev/null 2>&1 &";	
-					# replay gain
-					# runs oput of perl as a seperate parrallel proccess
-					if(! $TEST ){
-						$out = system($command); # ID3 tag content->"Audiobook"
-					}else{
-						print ">>Executing: $command\n";
-					}
-				}
-				&backup_files($file);
-			}
-			print "# Encoding complete\n#\n";
+		if(!$NORMALIZATION){
+			$lame_normalization_arguments = "--noreplaygain";
+		}elsif(!$USE_MP3GAIN_FOR_NORMALIZATION){
+			$lame_normalization_arguments = "--replaygain-accurate";
+		}else{
+			$lame_normalization_arguments = "--noreplaygain";
 		}
+		print "# Starting Lame...\n";
+		$lame_command = "lame --quite --resample 22050 --cbr -b 32 $lame_normalization_arguments \"$file\" \"$file2\"";
+		if(! $TEST ){
+			$out = `$lame_command`;
+		}else{
+			print ">>Executing: $lame_command\n";
+		}
+
+		if($? && !$TEST){ # lame encode failed
+			print "# ERROR encoding: $file \n";
+			print FILE "Fail: $file\n";
+			print "# Deleting newly created, failed file\n";
+			$out = `rm -f "$file2"`;
+			print "EXITING (1)\n";
+			exit 1;
+		}else{ #lame sucsseded
+			print FILE "Good: $file2\n";
+			if($ADD_ID3_AUDIOBOOK_TAG){
+				print "# Applying ID3 tag, \"Audiobook\" genre...\n";
+				$command = "id3v2 --TCON \"Audiobook\" \"$file2\"";
+				if(! $TEST ){
+					$out = `$command`; # ID3 tag content->"Audiobook"
+				}else{
+					print ">>Executing: $command\n";
+				}
+			}
+
+			if($USE_MP3GAIN_FOR_NORMALIZATION && $NORMALIZATION){
+				print "# Applying volume normilization...\n";
+				$command = "mp3gain \"$file2\" > /dev/null 2>&1 &";	
+				# replay gain
+				# runs oput of perl as a seperate parrallel proccess
+				if(! $TEST ){
+					$out = system($command); # ID3 tag content->"Audiobook"
+				}else{
+					print ">>Executing: $command\n";
+				}
+			}
+			&backup_files($file);
+		}
+		print "# Encoding complete\n#\n";
 	}
 }
 
@@ -281,6 +298,8 @@ if($USE_MP3GAIN_FOR_NORMALIZATION){
 	`mp3gain -v` || die "Error: 'mp3gain' not installed"
 }
 `avconv -version` || die "Error: 'avconv' not installed";
+`lame --version` || die "Error: 'mp3lame' not installed";
+
 if($ADD_ID3_AUDIOBOOK_TAG){
 	`id3v2 -v` || die "Error: 'id3v2' not installed";
 }
